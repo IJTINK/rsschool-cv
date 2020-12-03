229094f466074eb15ca4793769c47057eb91b1a8
IJTINK <i.just.trust.it.nk@gmail.com>
2020-12-03 20:36:50 +0300

    feat: edit singolo

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//burger
+
 let burger = document.querySelector(".burger_button");
 let burgerMenu = document.querySelector(".burger_menu");
 
@@ -5,16 +7,9 @@ let burgerServices = document.querySelector(".burger_services");
 let burgerPortfolio = document.querySelector(".burger_portfolio");
 let burgerHome = document.querySelector(".burger_home");
 
-let leftArrow = document.querySelector(".left_arrow");
-let rightArrow = document.querySelector(".right_arrow");
-
-
-
 let burgerMenuOut = () => {
-      burger.classList.toggle("rotate");
-      burgerMenu.classList.toggle("burger_menu_active");
-
-     
+  burger.classList.toggle("rotate");
+  burgerMenu.classList.toggle("burger_menu_active");
 };
 
 burger.addEventListener("click", burgerMenuOut);
@@ -23,51 +18,35 @@ burgerServices.addEventListener("click", burgerMenuOut);
 burgerHome.addEventListener("click", burgerMenuOut);
 
 
+// slider
+
+let leftArrow = document.querySelector(".left_arrow");
+let rightArrow = document.querySelector(".right_arrow");
+
+let redSlide = document.querySelector(".red_slide");
+let blueSlide = document.querySelector(".blue_slide");
 
 
 
-
-
-// let filterButtonAll = document.querySelectorAll(".portfolio-item");
-
-// filterButtonAll.forEach((item) => {
-//   item.addEventListener("click", (e) => {
-//     if (e.target.classList.contains("portfolio-item")) {
-//       removeClass();
-//       addClass(e);
-//       if (e.target.innerHTML === "All") {
-//         showAllImages();
-//       } else {
-//         setOrderImage(e.target.innerHTML);
-//       }
-//     }
-//   });
-// });
-
-// function addClass(e) {
-//   e.target.classList.add("portfolio_button_active");
+// let moveLeft = () => {
+//       line.style.transform = 'translateX(100%)';
 // }
 
-// function removeClass() {
-//   filterButtonAll.forEach((item) => {
-//     item.classList.remove("portfolio_button_active");
-//   });
-// }
 
-// function setOrderImage(tagname) {
-//   let images = document.querySelectorAll(".portfolio-image");
-//   images.forEach((picture) => {
-//     picture.classList.remove("order");
-//     if (picture.dataset.image === tagname) {
-//       picture.classList.add("order");
-//     }
-//   });
-// }
+// leftArrow.addEventListener("click", moveLeft);
 
-// function showAllImages() {
-//   let images = document.querySelectorAll(".portfolio-image");
-//   images.forEach((image) => {
-//     image.classList.remove("order");
-//   });
-// }
+let moveLeft = () => {
+      line.style.transform = 'translateX(-100%)';
+}
+
+
+leftArrow.addEventListener("click", moveLeft);
+
+
+let moveRight = () => {
+      line.style.transform = 'translateX(0)';
+}
+
+
+rightArrow.addEventListener("click", moveRight);
 
