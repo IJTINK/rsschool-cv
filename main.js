5f543184d3a960c7ac589da71fa374e25be53672
IJTINK <i.just.trust.it.nk@gmail.com>
2020-12-04 14:02:11 +0300

    feat: correction of Singolo

--- main.js
+++ main.js
@@ -22,31 +22,13 @@ burgerHome.addEventListener("click", burgerMenuOut);
 
 let leftArrow = document.querySelector(".left_arrow");
 let rightArrow = document.querySelector(".right_arrow");
-
 let redSlide = document.querySelector(".red_slide");
 let blueSlide = document.querySelector(".blue_slide");
 
 
-
-// let moveLeft = () => {
-//       line.style.transform = 'translateX(100%)';
-// }
-
-
-// leftArrow.addEventListener("click", moveLeft);
-
-let moveLeft = () => {
-      line.style.transform = 'translateX(-100%)';
-}
-
-
+let moveLeft = () => {line.style.transform = 'translateX(-100%)';}
 leftArrow.addEventListener("click", moveLeft);
-
-
-let moveRight = () => {
-      line.style.transform = 'translateX(0)';
-}
-
-
+let moveRight = () => {line.style.transform = 'translateX(0)';}
 rightArrow.addEventListener("click", moveRight);
+
 
