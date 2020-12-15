c56f03351e4fec2faf010e85e7c918db007f76d4
IJTINK <i.just.trust.it.nk@gmail.com>
2020-12-15 12:27:30 +0000

    feat: edit DynamicLandingPage

--- main.js
+++ main.js
@@ -4,35 +4,49 @@ let nameOfUser = document.getElementById("nameOfUser");
 let question = document.getElementById("question");
 let answer = document.getElementById("answer");
 
-function showTime(){
+
+
+
+
+
+let showTime = () => {
 
 let day = new Date();
 let hour = day.getHours();
 let minute = day.getMinutes();
 let second = day.getSeconds();
 
-hour = hour % 12 || 12; // если при делении на 12 есть остаток - то true, если 0 (false), то 12
 
-currentTime.innerHTML = `${hour}<span>:</span>${minute}<span>:</span>${second}`;
+currentTime.innerHTML = (hour < 10 ? "0" : '') + hour + ":" + (minute < 10 ? "0" : '') + minute + ":" + (second < 10 ? "0" : '') + second;
 setTimeout(showTime, 1000);
 }
 
 
-function changeBackgroundAndMeeting(){
+let changeBackgroundAndMeeting = () => {
 let day = new Date();
 let hour = day.getHours();
 
 if(hour < 12){
 document.body.style.backgroundImage = "url('/images/morning.jpg')";
+greeting.innerHTML = "Good morning,"
 }else
 if(hour < 18){
     document.body.style.backgroundImage = "url('/images/afternoon.jpg')";
+    greeting.innerHTML = "Good afternoon,"
 }else{
-    document.body.style.backgroundImage = "url('/images/evening.jpg')";
-    
+    document.body.style.backgroundImage = "url('/images/evening.jpg')";  
+    greeting.innerHTML = "Good evening,"
 }
 }
 
+// let inputName = () => {
+//     if(localStorage.getItem("nameOfUser") === null){
+//         nameOfUser.textContent = "Input your name";
+//     }else{
+//         nameOfUser.textContent = localStorage.getItem('nameOfUser');
+//     }
+// }
 
 showTime();
 changeBackgroundAndMeeting();
+// inputName();
