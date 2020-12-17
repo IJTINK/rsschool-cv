1682fa49b2ad9a68d55e151b7249dfcdafa7d771
IJTINK <i.just.trust.it.nk@gmail.com>
2020-12-17 14:36:22 +0000

    feat: change visual part of App

---
+++ main.js
@@ -0,0 +1,10 @@
+let audio = document.querySelector(".audio");
+let playAudio = document.querySelector(".play-button");
+let video = document.querySelector(".video");
+let songs = document.querySelectorAll(".weather-buttons button");
+let timeDisplay = document.querySelector(".time-display");
+
+playAudio.addEventListener('click', () => {
+    console.log('hi')
+    
+})
