9670a5e50f13c1f91053a6ceb2ea7ffb40cfddfe
IJTINK <i.just.trust.it.nk@gmail.com>
2020-12-21 19:37:54 +0000

    feat: change fuctional part

--- main.js
+++ main.js
@@ -5,19 +5,13 @@ let question = document.getElementById("question");
 let answer = document.getElementById("answer");
 
 
-
-
-
-
 let showTime = () => {
 
 let day = new Date();
 let hour = day.getHours();
 let minute = day.getMinutes();
 let second = day.getSeconds();
-
-
-currentTime.innerHTML = (hour < 10 ? "0" : '') + hour + ":" + (minute < 10 ? "0" : '') + minute + ":" + (second < 10 ? "0" : '') + second;
+currentTime.textContent = (hour < 10 ? "0" : '') + hour + ":" + (minute < 10 ? "0" : '') + minute + ":" + (second < 10 ? "0" : '') + second;
 setTimeout(showTime, 1000);
 }
 
@@ -28,25 +22,36 @@ let changeBackgroundAndMeeting = () => {
 
     if(hour < 12){
     document.body.style.backgroundImage = "url('/images/morning.jpg')";
-    greeting.innerHTML = "Good morning,"
+    greeting.textContent = "Good morning,"
     }else
     if(hour < 18){
         document.body.style.backgroundImage = "url('/images/afternoon.jpg')";
-        greeting.innerHTML = "Good afternoon,"
+        greeting.textContent = "Good afternoon,"
     }else{
         document.body.style.backgroundImage = "url('/images/evening.jpg')";  
-        greeting.innerHTML = "Good evening,"
+        greeting.textContent = "Good evening,"
     }
 }
 
-let inputName = () => {
-    if(localStorage.getItem("nameOfUser") === null){
-        nameOfUser.textContent = "Input your name";
-    }else{
-        nameOfUser.textContent = localStorage.getItem("nameOfUser");
-    }
-}
+// let inputName = () => {
+//     if(localStorage.getItem("nameOfUser") === null){
+//         nameOfUser.textContent = "Input your name";
+//     }else{
+//         nameOfUser.textContent = localStorage.getItem("nameOfUser");
+//     }
+// }
+
+// let setName = (e) =>{
+//     if (e.type === "keydown"){
+//         if(e.which == 13 || e.key == 13){
+//             localStorage.setItem('nameOfUser', e.target.innerText);
+//             nameOfUser.blur();
+//         }
+//     }else{
+//         localStorage.setItem('nameOfUser', e.target.innerText);
+//     }
+// }
 
 showTime();
 changeBackgroundAndMeeting();
-inputName();
+// nameOfUser.addEventListener("keydown", inputName);
