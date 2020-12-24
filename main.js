b8ea71afc0f69a69a47b268eaa50c31d696ba723
IJTINK <i.just.trust.it.nk@gmail.com>
2020-12-24 00:08:05 +0000

    change functional part of main.js

--- main.js
+++ main.js
@@ -1,2 +1,23 @@
-let audio = document.querySelectorAll(".audio");
+let audios = document.querySelectorAll(".audio");
 let chords = document.querySelectorAll(".chord");
+
+let playAudio = (key) => {
+    for (let audio of audios) {
+        if (audio.getAttribute("data-key") == key) {
+            audio.load();
+            audio.play();
+            break;
+        }
+    }
+}
+
+chords.forEach((chord) => {
+    chord.addEventListener("click",() => {
+        let key = chord.getAttribute("data-key");
+        playAudio(key);
+    });
+});
+document.addEventListener("keydown", (e) => {
+    let key = e.which;
+    playAudio(key);
+});
